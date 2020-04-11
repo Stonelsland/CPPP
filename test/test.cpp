f86990ba4bcf6dea4bf58bc375c09fdc3bf42938
Phil <852217352@qq.com>
2020-04-11 22:17:17 +0800

    srand_func_test

--- test/test.cpp
+++ test/test.cpp
@@ -4,9 +4,9 @@
 
 #include "test.h"
 int main(){
-    srand(5);
+    srand(5); //seed不同，每次输出的随机数就不同
     for (int i = 0; i < 15; ++i){
-        int j = rand() % 6;
+        int j = rand() % 6; // rand()用来限制输出随机数的范围，变换seed，就变换一组随机数
         cout << j << '\n';
     }
 
