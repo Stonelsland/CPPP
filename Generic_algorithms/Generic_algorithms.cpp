41e4517eb4de6f671e72ffd8697e42c0c0c4bdee
NOAH <1090904079@qq.com>
2020-04-18 23:23:12 +0800

    顺序性容器与泛型算法的使用

--- Generic_algorithms/Generic_algorithms.cpp
+++ Generic_algorithms/Generic_algorithms.cpp
@@ -2,8 +2,10 @@
 // Created by NOAH on 2020/4/14.
 //
 
-#include "iterator_demo.h"
-int count_occurs(const vector<int> &vec,int val);
+#include "Generic_algorithms.h"
+
+int count_occurs(const vector<int> &vec, int val);
+
 int main() {
     const int asize = 8;
     int a[asize] = {1, 1, 2, 3, 5, 8, 13, 21};
@@ -12,15 +14,15 @@ int main() {
     vector<int> ivec(a, a + asize);
     list<int> ilist(a, a + asize);
 
-    int *pia = find(a, a + asize, 21);
+    int *pia = find_out(a, a + asize, 21);
     if (pia != a + asize) cout << "找到了 " << *pia << endl;
 
     vector<int>::iterator it;
-    it = find(ivec.begin(), ivec.end(), 21);
+    it = find_out(ivec.begin(), ivec.end(), 21);
     if (it != ivec.end()) cout << "找到了 " << *it << endl;
 
     list<int>::iterator iter;
-    iter = find(ilist.begin(), ilist.end(), 21);
+    iter = find_out(ilist.begin(), ilist.end(), 21);
     if (iter != ilist.end()) cout << "找到了 " << *iter << endl;
     // insert
     iter = ilist.begin();
@@ -42,7 +44,7 @@ int main() {
     string sval = ("Part Two");
     list<string> slist;
     slist.push_back(sval);
-    list<string>::iterator istr = find(slist.begin(), slist.end(), sval);
+    list<string>::iterator istr = find_out(slist.begin(), slist.end(), sval);
     if (istr != slist.end()) cout << "找到了 " << *istr << endl;
     slist.insert(istr, 8, "dummy");
     istr = slist.begin();
@@ -55,15 +57,29 @@ int main() {
     int ia2[5] = {18, 25, 33, 40, 44};
     list<int> elems(ia1, ia1 + 9);
 
-    list<int>::iterator itia = find(elems.begin(), elems.end(), 55);
+    list<int>::iterator itia = find_out(elems.begin(), elems.end(), 55);
     elems.insert(itia, ia2, ia2 + 5);
     itia = elems.begin();
     for (; itia != elems.end(); itia++) {
         cout << *itia << " ";
     }
 
-    //
-    int ia3[7] = {6,10,8,4,10,7,10};
-    vector<int> vec(ia3,ia3+7);
-    cout << endl << "共有 " <<count_occurs(vec,10)<<" 个";
+    //调用泛型filter
+    int ia3[7] = {6, 10, 8, 4, 10, 7, 10};
+    vector<int> vec(ia3, ia3 + 7);
+    cout << endl << "共有 " << count_occurs(vec, 10) << " 个";
+
+    const int elem_size = 8;
+    int ia4[elem_size] = {12,8,43,0,6,21,3,7};
+    vector<int> ivec1(ia4,ia4+elem_size);
+
+    int ia5[elem_size];
+    vector<int> ivec2(elem_size);
+
+    cout << "filtering intager array for values less than 8 \n";
+    filter(ia4,ia4+elem_size,ia5,elem_size,less<int>());
+
+    cout << "filtering intager array for values greater than 8 \n";
+    filter(ivec1.begin(),ivec1.end(),ivec2.begin(),elem_size,greater<int>());
+
 }
---
+++ Generic_algorithms/Generic_algorithms.h
@@ -0,0 +1,81 @@
+//
+// Created by NOAH on 2020/4/14.
+//
+
+#ifndef ITERATOR_H
+#define ITERATOR_H
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+class Generic_algorithms {
+
+};
+
+template<typename IteratorType, typename elemType>
+IteratorType
+find_out(IteratorType first, IteratorType last,
+         const elemType &value) {
+    for (; first != last; ++first) {
+        if (value == *first)
+            return first;
+    }
+    return last;
+}
+
+int count_occurs(const vector<int> &vec, int val) {
+    vector<int>::const_iterator iter = vec.begin();
+    int occurs_count = 0;
+    while ((iter = find(iter, vec.end(), val)) != vec.end()) {
+        ++occurs_count;
+        //这里的++iter 是为了保证'不对任一元素进行两次以上的检视'
+        ++iter;
+    }
+    return occurs_count;
+}
+
+/*
+ * 标准库提供了两个binder adapter
+ * bind1st：将指定值绑定至第一操作数
+ * bind2nd：将指定值绑定至第二操作数
+ */
+vector<int> filter(const vector<int> &vec,
+                   int val, less<int> &lt) {
+    vector<int> nvec;
+    vector<int>::const_iterator iter = vec.begin();
+    /*
+     * bind2nd(less<int> , val)
+     * 会把val绑定于less<int>的第二个参数上
+     * 于是，less<int>会将每个元素拿来与val比较
+     */
+    while ((iter = find_if(iter, vec.end(),
+                           bind2nd(lt, val))) != vec.end()) {
+        nvec.push_back(*iter);
+        iter++;
+    }
+    return nvec;
+}
+
+/*
+ * 消除filter与vector的型别关联，使filter泛型化
+ * 消除与容器类型的相关性，传入一对iterators[first,last]，
+ * 并在参数中增加另一个iterator，用以指定从何处开始复制元素
+ */
+template<typename InputIterator, typename OutputIterator,
+        typename ElemType, typename Comp>
+OutputIterator filter(InputIterator first, InputIterator last,
+                      OutputIterator opi, const ElemType &val, Comp pred) {
+    while ((first = find_if(first, last, bind2nd(pred, val))) != last) {
+        cout << "found value: " << *first << endl;
+        *opi++ = *first++;
+    }
+    return opi;
+}
+
+
+#endif //ITERATOR_H
