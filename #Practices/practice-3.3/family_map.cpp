4ef03e9b8ff8b0bfa3669f84bb28d2303a355297
noahliu <noahliu@canway.net>
2020-04-20 19:08:58 +0800

    practice-3.3

---
+++ #Practices/practice-3.3/family_map.cpp
@@ -0,0 +1,5 @@
+//
+// Created by NOAH on 2020/4/20.
+//
+
+#include "family_map.h"
---
+++ #Practices/practice-3.3/family_map.h
@@ -0,0 +1,20 @@
+//
+// Created by NOAH on 2020/4/20.
+//
+
+#ifndef FAMILY_MAP_H
+#define FAMILY_MAP_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+class family_map {};
+
+void populate_map(const map<string,string> &vec,)
+
+#endif //FAMILY_MAP_H
